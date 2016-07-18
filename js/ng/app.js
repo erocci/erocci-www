0788f5d6102aa7840f0e1375a495c0e3bee16856
Jean Parpaillon <jean.parpaillon@free.fr>
2016-07-18 17:44:14 +0200

    Doc points to github

--- js/ng/app.js
+++ js/ng/app.js
@@ -10,11 +10,10 @@ angular.module('erocciApp', [
     'hljs',
     'ngResource',
     //'duScroll',
-    'ui.router',
-    'hc.marked'
+    'ui.router'
 ])
-    .config(['$stateProvider', '$urlRouterProvider', 'markedProvider',
-	     function($stateProvider, $urlRouterProvider, markedProvider) {
+    .config(['$stateProvider', '$urlRouterProvider',
+	     function($stateProvider, $urlRouterProvider) {
 		 $urlRouterProvider.otherwise('/main');
 
 		 $stateProvider
@@ -22,46 +21,6 @@ angular.module('erocciApp', [
 			 url: '/main',
 			 templateUrl: 'partials/main.html',
 			 data: { pageTitle: 'Home' }
-		     })
-		     .state('getstarted', {
-			 url: '/getstarted',
-			 templateUrl: 'partials/getstarted.html',
-			 data: { pageTitle: 'Getting Started' }
-		     })
-		     .state('reference', {
-			 url: '/devref',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
-		     })
-		     .state('ref_model', {
-			 url: '/devref#model',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
-		     })
-		     .state('ref_capabilities', {
-			 url: '/devref#capabilities',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
-		     })
-		     .state('ref_http', {
-			 url: '/devref#http',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
-		     })
-		     .state('ref_xmpp', {
-			 url: '/devref#xmpp',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
-		     })
-		     .state('ref_dbus', {
-			 url: '/devref#dbus',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
-		     })
-		     .state('ref_listener', {
-			 url: '/devref#listener',
-			 templateUrl: 'partials/devref.html',
-			 data: { pageTitle: 'Developer Reference' }
 		     })
 		     .state('download', {
 			 url: '/download',
@@ -98,11 +57,4 @@ angular.module('erocciApp', [
 			 templateUrl: 'partials/credits.html',
 			 data: { pageTitle: 'Credits' }
 		     });
-
-		 markedProvider.setRenderer({
-		     link: function(href, title, text) {
-			 console.log("LINK: " + href);
-			 return "<a href='" + href + "'" + (title ? " title='" + title + "'" : '') + " >" + text + "</a>";
-		     }
-		 });
 	     }]);
